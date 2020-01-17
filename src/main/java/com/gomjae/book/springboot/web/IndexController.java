7ee05968fe763edacf9a2d71f3a55cf5f8a8d54a
ggomjae <ggomjae@naver.com>
2020-01-17 20:36:38 +0900

    7th commit
    1. DB 설정
    2. application-real.properties 설정

--- src/main/java/com/gomjae/book/springboot/web/IndexController.java
+++ src/main/java/com/gomjae/book/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.gomjae.book.springboot.web;
 
 
+import com.gomjae.book.springboot.config.auth.LoginUser;
+import com.gomjae.book.springboot.config.auth.dto.SessionUser;
 import com.gomjae.book.springboot.service.PostsService;
 import com.gomjae.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +18,13 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
 
         return "index";
     }
