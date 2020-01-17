234dad98c0be34c5dd29e9954982281e1495419a
ggomjae <ggomjae@naver.com>
2020-01-17 13:54:49 +0900

    3th commit
    1.gitignore - oauth 확인

---
+++ src/test/java/com/gomjae/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.gomjae.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트게시물";
+        String content = "테스트본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("gomjae@naver.com")
+                .build());
+    }
+}
