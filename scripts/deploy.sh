b96cd6197cd392b80aac69c93ba4fa9bfc62cbfc
ggomjae <ggomjae@naver.com>
2020-01-20 19:32:48 +0900

    22th commit
    1. deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,12 @@ PROJECT_NAME=springbootservice
 
 echo "> Build 파일 복사"
 
-cd $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl springboot-service-project | grep jar | awk '{print $1}')
 
-echo "현재 구동중인 애플리케이션 pid : $CURRENT_PID"
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
